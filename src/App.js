ca96e50797c569668e7d8a9e5b9aeca565cdcd5a
Poliarine <93043832+Poliarine@users.noreply.github.com>
2022-05-27 15:47:12 +0300

    App.js papildymas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Button from "./components/Button";
+import "./App.css";
 
 function App() {
+  const [randomQuote, setRandomQuote] = useState([]);
+  const [randomAuthor, setRandomAuthor] = useState([]);
+
+  useEffect((props) => {
+    fetch("https://type.fit/api/quotes")
+      .then(function (response) {
+        return response.json();        
+      })
+      .then(function (quotes) {
+        const randomQuoteGet = quotes[Math.floor(Math.random()*quotes.length)];
+        console.log(randomQuoteGet);
+
+        let randomQ = randomQuoteGet.text;
+        let randomA = randomQuoteGet.author;
+        
+        console.log(randomQ);
+        console.log(randomA);
+        
+        setRandomQuote(props.text.value)
+        setRandomAuthor(props.target.value)
+      });
+  }, [randomQuote, randomAuthor]);
+
+ 
+  let handleClick = (event) => {
+    //const randomQuoteGet = quotes[Math.floor(Math.random()*quotes.length)];
+    //var joke = jokes[Math.floor(Math.random()*jokes.length)];
+    setRandomQuote('"Genius is one percent inspiration and ninety-nine percent perspiration."')
+    setRandomAuthor('Thomas Edison');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div class="wrapper" id="quote-box" className="App">
+      <h1>Random quote machine</h1>
+      <div class="quote-div">
+        <p id="text">{randomQuote}</p>
+        <div>
+          <p id="author">{randomAuthor}</p>
+        </div>
+      </div>
+      <div class="buttons">
+        <a id="tweet-quote" href="www">
+          Twitter
         </a>
-      </header>
+        <a id="social-quote" href="www">
+          Social media
+        </a>
+        <Button onClick={handleClick} id="new-quote">
+          New quote
+        </Button>
+      </div>
     </div>
   );
 }
